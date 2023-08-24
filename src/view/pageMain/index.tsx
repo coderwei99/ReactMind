a92c98bd51f997e28b8498fa474c8d645a8044c8
coderwei99 <916606569@qq.com>
2023-08-24 22:43:46 +0800

    fix: set mind container padding

--- src/view/pageMain/index.tsx
+++ src/view/pageMain/index.tsx
@@ -4,7 +4,7 @@ import { defaultNode } from '@/static'
 export default function index() {
   // console.log('defaultNode', defaultNode)
   return (
-    <div className='relative flex justify-center items-center h-full'>
+    <div className='relative flex justify-center items-center h-full px-[30vw] py-[30vh]'>
       {/* 思维导图组件容器 */}
       <MainMapContainer defaultNode={defaultNode[0]}></MainMapContainer>
     </div>
