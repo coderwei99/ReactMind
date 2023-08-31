a837b2fcaf0d6341017b2e8fa2fa5bd6d5f9e81c
coderwei99 <916606569@qq.com>
2023-09-01 02:26:00 +0800

    feat: fixed page nav bar

--- src/view/pageNav/index.tsx
+++ src/view/pageNav/index.tsx
@@ -7,11 +7,11 @@ export default function Template() {
   const navLeftConfig = pageNavConfig.slice(0, mid)
   const navRightConfig = pageNavConfig.slice(mid)
   return (
-    <div className='border-b border-b-zinc-300 h-[48px] w-full flex justify-between'>
+    <div className='border-b border-b-zinc-300 h-[48px] w-full flex justify-between' style={{ position: 'fixed' }}>
       <NavLeft navLeftConfig={navLeftConfig} />
       <NavTitle />
       {/* <NavRight/> */}
-      <NavLeft navLeftConfig = {navRightConfig}/>
+      <NavLeft navLeftConfig={navRightConfig} />
     </div>
   )
 }
