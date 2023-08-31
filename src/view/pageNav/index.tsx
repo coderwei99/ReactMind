dd701e22c7fb9302832746854c1294d2e174b5ae
coderwei99 <916606569@qq.com>
2023-09-01 02:28:12 +0800

    style: add comments

--- src/view/pageNav/index.tsx
+++ src/view/pageNav/index.tsx
@@ -7,6 +7,7 @@ export default function Template() {
   const navLeftConfig = pageNavConfig.slice(0, mid)
   const navRightConfig = pageNavConfig.slice(mid)
   return (
+    // todo: 这里用tailwindcss 的fixed 原子类会导致内部的click事件失效 但是用普通的fixed就不会 不知道是不是tailwindcss的问题 待研究
     <div className='border-b border-b-zinc-300 h-[48px] w-full flex justify-between' style={{ position: 'fixed' }}>
       <NavLeft navLeftConfig={navLeftConfig} />
       <NavTitle />
